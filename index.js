e5a836292c65e01542b7f9f8d147a0e3ad0e6b2c
ritobrotos <rito.rcks@gmail.com>
2017-08-18 01:09:09 +0530

    Change

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-var params = {
-    q: 'market', count: 2
-};
+var twitterAccounts = [
+	'CNBCTV18Live',
+	'moneycontrolcom',
+	'ETNOWlive'
+];
 
-function gotData(err, data, response) {
-    var tweets = data.statuses;
-    for (var i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].text);
-    }
+for (var i = 0; i < twitterAccounts.length; i++) {
+	var params = {
+    	q: 'from:' + twitterAccounts[i], 
+    	count: 5, result_type: 'recent'
+	};
+
+	function gotData(err, data, response) {
+	    var tweets = data.statuses;
+	    for (var j = 0; j < tweets.length; j++) {
+	    	console.log(twitterAccounts[i] + '::');
+	        console.log(tweets[j].text + '\n');
+	    }
+	}
+
+	T.get('search/tweets', params, gotData);
 
 }
-
-T.get('search/tweets', params, gotData);
