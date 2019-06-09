e0def1f3589c908520902e8b7d0ec07a778f4862
Ritobroto Seth <rito.rcks@gmail.com>
2019-06-10 00:33:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key:         'DeQTs95NgaEt8WzpByoV5sH0Q',
-    consumer_secret:      'fbZ59E0U2KEyA3ziPKFPBUjNUgHSkxWD8r4QGlh6GAlaLixF9Y',
-    access_token:         '2879142961-areTHbOlhFgsrUc4IJaRSfHtH9ppZwVRNHQf6Wp',
-    access_token_secret:  'UlOhM1QWrI2NSIK6kHiXqn3IU4suJkJoKJ93X844AYihu',
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
